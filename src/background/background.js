587489bfc6a2b3717da7996ae275eacf99db735b
amahdavid <amahdavid34@gmail.com>
2024-04-10 16:33:16 -0700

    CS: added buttons for moving left and right, added commands for moving left and right

--- src/background/background.js
+++ src/background/background.js
@@ -1,23 +1,8 @@
-function sortTabsAlphabeticallyBackGround(sortDirection) {
-  chrome.tabs.query({}, function (tabs) {
-    tabs.sort(function (a, b) {
-      if (sortDirection === "asc") {
-        return a.title.localeCompare(b.title);
-      } else {
-        return b.title.localeCompare(a.title);
-      }
-    });
-
-    tabs.forEach(function (tab, index) {
-      chrome.tabs.move(tab.id, { index: index });
-    });
-  });
-}
+import { commandMap } from "../assets/commandsList.js";
 
 chrome.commands.onCommand.addListener(function (command) {
-  if (command === "arrange_tabs_alphabetically_ascending") {
-    sortTabsAlphabeticallyBackGround("asc");
-  } else if (command === "arrange_tabs_alphabetically_descending") {
-    sortTabsAlphabeticallyBackGround("desc");
+  const action = commandMap[command];
+  if (action) {
+    action();
   }
 });
--- src/options/index.js
+++ src/options/index.js
@@ -1,4 +1,4 @@
-function sortTabsAlphabetically(sortDirection) {
+export function sortTabsAlphabetically(sortDirection) {
   chrome.tabs.query({}, function (tabs) {
     tabs.sort(function (a, b) {
       if (sortDirection === "asc") {
@@ -14,10 +14,34 @@ function sortTabsAlphabetically(sortDirection) {
   });
 }
 
+export function navigateTabs(offset) {
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (activeTabs) {
+      var currentIndex = tabs.findIndex(tab => tab.id === activeTabs[0].id);
+      var newIndex = currentIndex + offset;
+
+      if (newIndex >= 0 && newIndex < tabs.length) {
+        chrome.tabs.update(tabs[newIndex].id, { active: true });
+      }
+    });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   var arrangeButton = document.getElementById("arrange-btn");
+  var nextButton = document.getElementById("next-btn");
+  var previousButton = document.getElementById("previous-btn");
+
   arrangeButton.addEventListener("click", function () {
     var sortDirection = document.getElementById("sort-direction").value;
     sortTabsAlphabetically(sortDirection);
+  });
+
+  nextButton.addEventListener("click", function () {
+    navigateTabs(1);
+  });
+
+  previousButton.addEventListener("click", function () {
+    navigateTabs(-1);
   });
 });
