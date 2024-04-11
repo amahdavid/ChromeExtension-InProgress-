bbc6dc3675f6d3c37ec37693e0c2e92c042fcac5
amahdavid <amahdavid34@gmail.com>
2024-04-10 22:52:17 -0700

    CS: created readme, finished jest set up

--- tests/jest_test/unit.test.js
+++ tests/jest_test/unit.test.js
@@ -1,15 +1,14 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-describe('Extension Commands', () => {
+describe("Extension Commands", () => {
   let browser;
   let page;
-  let extensionId = 'nnipdgnpmhjblcglpglehlfaopjmffen';
+  let extensionId = "nnipdgnpmhjblcglpglehlfaopjmffen";
 
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
       slowMo: 50,
-      devtools: true,
       args: [
         `--disable-extensions-except=${__dirname}/../..`,
         `--load-extension=${__dirname}/../..`,
@@ -24,27 +23,32 @@ describe('Extension Commands', () => {
   });
 
   // Test arrange_tabs_alphabetically_ascending command
-  it('SortTabs_Ascending_HappyPath', async () => {
-    await page.keyboard.press('Y', { modifiers: ['Control', 'Shift'] });
-    debugger;
-    // Add assertions here to verify that tabs are arranged alphabetically in ascending order
+  test("SortTabs_Ascending_HappyPath", async () => {
+    await page.keyboard.press("Y", { modifiers: ["Control", "Shift"] });
+
+    const tabs = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll(".tab")).map((tab) =>
+        tab.textContent.trim()
+      );
+    });
+    expect(tabs).toEqual([...tabs].sort());
   });
 
   // Test arrange_tabs_alphabetically_descending command
-  it('SortTabs_Descending_HappyPath', async () => {
-    await page.keyboard.press('Z', { modifiers: ['Control', 'Shift'] });
+  test("SortTabs_Descending_HappyPath", async () => {
+    await page.keyboard.press("Z", { modifiers: ["Control", "Shift"] });
     // Add assertions here to verify that tabs are arranged alphabetically in descending order
   });
 
   // Test move_tab_previous command
-  it('SwitchTabs_Previous_HappyPath', async () => {
-    await page.keyboard.press('P', { modifiers: ['Control', 'Shift'] });
+  test("SwitchTabs_Previous_HappyPath", async () => {
+    await page.keyboard.press("P", { modifiers: ["Control", "Shift"] });
     // Add assertions here to verify that the active tab has changed
   });
 
   // Test move_tab_next command
-  it('SwitchTabs_Next_HappyPath', async () => {
-    await page.keyboard.press('O', { modifiers: ['Control', 'Shift'] });
+  test("SwitchTabs_Next_HappyPath", async () => {
+    await page.keyboard.press("O", { modifiers: ["Control", "Shift"] });
     // Add assertions here to verify that the active tab has changed
   });
 
